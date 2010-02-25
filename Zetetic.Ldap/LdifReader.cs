87e06ce60e374263d5da08edfc9a6495463bb105
skradel <skradel@zetetic.net>
2010-02-24 20:48:46 -0500

    Roughing out LDIF changetype support - not yet fully functional

--- Zetetic.Ldap/LdifReader.cs
+++ Zetetic.Ldap/LdifReader.cs
@@ -5,6 +5,7 @@ using System.IO;
 
 namespace Zetetic.Ldap
 {
+    public delegate void DeleteEntryEventHandler(object sender, DnEventArgs e);
     public delegate void EndEntryEventHandler(object sender, DnEventArgs e);
     public delegate void BeginEntryEventHandler(object sender, DnEventArgs e);
     public delegate void AttributeEventHandler(object sender, AttributeEventArgs e);
@@ -21,6 +22,15 @@ namespace Zetetic.Ldap
             this.DistinguishedName = dn;
         }
     }
+
+    /// <summary>
+    /// The state of the entire LDIF; not allowed to mix "full entry" and "changetype".
+    /// </summary>
+    public enum LdifType { Unknown, Normal, Changetype }
+
+    public enum ChangeType { Unknown, Add, Modify, Delete, ModRdn }
+
+    public enum AttributeOperationDirective { Unknown, Add, Replace, Delete, NewRdn, DeleteOldRdn, NewSuperior }
 
     /// <summary>
     /// LdifReader raises AttributeEventArgs upon reading a complete single attribute value
@@ -47,9 +57,17 @@ namespace Zetetic.Ldap
         private TextReader _source;
         private bool _ownsStream;
         private bool _openEntry;
-        private long _lineNum;
+        private long _lineNum, _endLineNum;
+
+        public LdifType LdifType { get; protected set; }
 
         public string LastDn { get; protected set; }
+
+        public string LastAttribute { get; protected set; }
+
+        public AttributeOperationDirective AttributeOperationDirective { get; set; }
+
+        public ChangeType EntryChangeType { get; protected set; }
 
         /// <summary>
         /// Some LDIF implementations might use more than one space on folded lines.  If TrimFoldedLines
@@ -58,11 +76,18 @@ namespace Zetetic.Ldap
         /// </summary>
         public bool TrimFoldedLines { get; set; }
 
+        #region Constructors
+
+        private LdifReader()
+        {
+            this.LdifType = LdifType.Unknown;
+        }
+
         /// <summary>
         /// Open the ASCII-encoded LDIF file at 'path' for reading.
         /// </summary>
         /// <param name="path"></param>
-        public LdifReader(string path)
+        public LdifReader(string path) : this()
         {
             _source = new StreamReader(new FileStream(path, FileMode.Open, FileAccess.Read), Encoding.ASCII);
             _ownsStream = true;
@@ -73,6 +98,7 @@ namespace Zetetic.Ldap
         /// </summary>
         /// <param name="sr"></param>
         public LdifReader(TextReader sr)
+            : this()
         {
             _source = sr;
             _ownsStream = false;
@@ -82,15 +108,18 @@ namespace Zetetic.Ldap
         /// Read from an already-open FileStream, assumed to be ASCII-encoded.
         /// </summary>
         /// <param name="fs"></param>
-        public LdifReader(FileStream fs)
+        public LdifReader(FileStream fs) : this()
         {
             _source = new StreamReader(fs, Encoding.ASCII);
             _ownsStream = false;
         }
+        #endregion
 
         public event EndEntryEventHandler OnEndEntry;
         public event BeginEntryEventHandler OnBeginEntry;
         public event AttributeEventHandler OnAttributeValue;
+
+        public event DeleteEntryEventHandler OnDeleteEntry;
 
         /// <summary>
         /// Raise any events from reading the next set of instructions from the LDIF.
@@ -99,21 +128,20 @@ namespace Zetetic.Ldap
         /// <returns>True if there is still more to read, false if EOF</returns>
         public bool Read()
         {
-            string line = _source.ReadLine();
+            string line = ReadLogicalLine();
 
             if (line != null)
             {
-                _lineNum++;
-
                 if (line.StartsWith("#"))
                 {
-                    // Skip comment line
+                    // Skip comment line; don't alter this.LastAttribute
                     return true;
                 }
 
                 if (!_openEntry && !line.StartsWith("dn:", StringComparison.InvariantCultureIgnoreCase))
                 {
                     // Preamble stuff, such as version: 1
+                    this.LastAttribute = "OTHER";
                     return true;
                 }
             }
@@ -126,83 +154,177 @@ namespace Zetetic.Ldap
 
                     if (OnEndEntry != null)
                         OnEndEntry(this, new DnEventArgs(this.LastDn));
+
+                    this.LastAttribute = null;
                 }
 
                 // End of file?
                 return (line != null);
             }
 
+            //--- Handle main cases ---
             if (line.StartsWith("dn:", StringComparison.InvariantCultureIgnoreCase))
             {
                 _openEntry = true;
 
-                string dn;
+                line = line.Substring(3);
 
-                if (line.TrimEnd(' ').ToLowerInvariant() == "dn:")
-                {
-                    dn = null;
-                }
-                else
-                {
-                    bool b64 = (line[3] == ':');
-                    dn = line.Substring(b64 ? 4 : 3).TrimStart();
+                this.LastDn = this.ParseAttributeValue(line);
 
-                    while (_source.Peek() == (int)' ')
-                        if (this.TrimFoldedLines)
-                            dn += _source.ReadLine().Substring(1).TrimStart(' ');
-                        else
-                            dn += _source.ReadLine().Substring(1);
-
-                    if (b64) dn = Encoding.UTF8.GetString(Convert.FromBase64String(dn));
-                }
-
-                this.LastDn = dn;
+                this.LastAttribute = "DN";
 
                 if (OnBeginEntry != null)
-                    OnBeginEntry(this, new DnEventArgs(dn));
+                    OnBeginEntry(this, new DnEventArgs(this.LastDn));
+            }
+            else if (line.StartsWith("changetype:", StringComparison.InvariantCultureIgnoreCase))
+            {
+                if (!"DN".Equals(this.LastAttribute))
+                    throw new ApplicationException("changetype must follow DN");
 
+                if (this.LdifType == LdifType.Unknown)
+                {
+                    // Possibly raise an event here - detection of the file type
+                    this.LdifType = LdifType.Changetype;
+                }
+                else if (this.LdifType == LdifType.Normal)
+                {
+                    throw new ApplicationException("Cannot mix changetype and normal content");
+                }
+
+                string chtype = ParseAttributeValue(line.Substring(11));
+                switch (chtype)
+                {
+                    case "add":
+                        this.EntryChangeType = ChangeType.Add;
+                        break;
+
+                    case "delete":
+                        this.EntryChangeType = ChangeType.Delete;
+                        break;
+
+                    case "moddn":
+                    case "modrdn":
+                        this.EntryChangeType = ChangeType.ModRdn;
+                        break;
+
+                    case "modify":
+                        this.EntryChangeType = ChangeType.Modify;
+                        break;
+
+                    default:
+                        throw new ApplicationException("Unknown changetype " + chtype + " at line " + _lineNum);
+                }
+            }
+            else if ("-".Equals(line))
+            {
+                if (this.LdifType != LdifType.Changetype)
+                    throw new ApplicationException("Encountered '-' in non-changetype LDIF");
+
+                // @todo Raise new event OnEndAttributeChanges
             }
             else
             {
                 int fc = line.IndexOf(':');
 
-                // Note that the parser will currently NOT read changetype-oriented
-                // LDIF ('-' on a line by itself between attr instructions).
                 if (fc == -1)
                     throw new ApplicationException("Malformed LDIF on line " + _lineNum);
 
                 string attrName = line.Substring(0, fc);
+                string attrVal = ParseAttributeValue(line.Substring(fc + 1));
 
-                string attrVal;
-                bool b64 = false;
-
-                if (fc + 1 == line.Length)
+                AttributeOperationDirective op;
+                if (this.LdifType == LdifType.Changetype && IsChangeDirective(attrVal, out op))
                 {
-                    attrVal = null;
+                    this.AttributeOperationDirective = op;
                 }
                 else
                 {
-                    b64 = (line[fc + 1] == ':');
+                    this.LastAttribute = attrName;
 
-                    attrVal = line.Substring(b64 ? fc + 2 : fc + 1).TrimStart();
-
-                    while (_source.Peek() == (int)' ')
-                        if (this.TrimFoldedLines)
-                            attrVal += _source.ReadLine().Substring(1).TrimStart(' ');
-                        else
-                            attrVal += _source.ReadLine().Substring(1);
-                }
-
-                if (OnAttributeValue != null)
-                {
-                    if (b64)
-                        OnAttributeValue(this, new AttributeEventArgs(attrName, Convert.FromBase64String(attrVal)));
-                    else
+                    if (OnAttributeValue != null)
                         OnAttributeValue(this, new AttributeEventArgs(attrName, attrVal));
+                    
                 }
             }
 
             return true;
+        }
+
+        protected bool IsChangeDirective(string attrVal, out AttributeOperationDirective op)
+        {
+            switch (attrVal)
+            {
+                case "add":
+                    op = AttributeOperationDirective.Add;
+                    return true;
+
+                case "replace":
+                    op = AttributeOperationDirective.Replace;
+                    return true;
+
+                case "delete":
+                    op = AttributeOperationDirective.Delete;
+                    return true;
+
+                case "newrdn":
+                    op = AttributeOperationDirective.NewRdn;
+                    return true;
+
+                case "deleteoldrdn":
+                    op = AttributeOperationDirective.DeleteOldRdn;
+                    return true;
+
+                case "newsuperior":
+                    op = AttributeOperationDirective.NewSuperior;
+                    return true;
+
+                default:
+                    op = AttributeOperationDirective.Unknown;
+                    return false;
+            }
+        }
+
+        private string ParseAttributeValue(String attr)
+        {
+            if (string.IsNullOrEmpty(attr) || attr.Trim().Length == 0)
+                return null;
+
+            if (attr[0] == ':')
+            {
+                return Encoding.UTF8.GetString(Convert.FromBase64String(attr.Substring(1).Trim()));
+            }
+            else
+            {
+                return attr.Substring(1).Trim();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private String ReadLogicalLine()
+        {
+            _lineNum = ++_endLineNum;
+
+            string line = _source.ReadLine();
+            
+            if (!string.IsNullOrEmpty(line))
+            {
+                //Can be continued if it starts with a space
+                while (_source.Peek() == (int)' ')
+                {
+                    _endLineNum++;
+
+                    String extra = _source.ReadLine().Substring(1);
+
+                    if (this.TrimFoldedLines)
+                        extra = extra.TrimStart(' ');
+                    
+                    line += extra;
+                }
+            }
+            return line;
         }
 
         /// <summary>
